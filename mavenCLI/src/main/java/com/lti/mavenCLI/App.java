6b22015458918e37ae83a3f30a091d8a801b8dfd
balaji <balajidontul7271@gmail.com>
2021-07-29 16:12:50 +0530

    first

---
+++ mavenCLI/src/main/java/com/lti/mavenCLI/App.java
@@ -0,0 +1,13 @@
+package com.lti.mavenCLI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
